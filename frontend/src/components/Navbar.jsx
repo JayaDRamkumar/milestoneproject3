20e3dc28f21cfa2598f07b1aeda96d1d6de3c003
“JayaDRamkumar” <jayadramkumar@gmail.com>
2023-11-28 22:18:23 -0500

    movestylesheet

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,6 @@ function AppNavbar() {
   return (
 
     <Navbar bg="dark" variant="dark">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
       <Navbar.Brand as={Link} to="/">LR LAKE HOLDING INC</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link as={Link} to="/">Home</Nav.Link>
